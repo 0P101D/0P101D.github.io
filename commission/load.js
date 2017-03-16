17d137c420dbd54c00bbc456727a89827b94ef8a
cae <duologist@gmail.com>
2017-03-16 11:58:52 -0400

    commissions

---
+++ commission/load.js
@@ -0,0 +1,22 @@
+$('#gallery').load("gallery.html");
+$('.commtype span').load("commtype.html");
+$('.rules span').load("rules.html");
+$('.form span').load("form.html");
+$('.good span').load("good.html");
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
