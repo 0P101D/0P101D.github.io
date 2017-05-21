66ba76eef806baa19d3ecdee08ec2d25eb0b594f
cae <duologist@gmail.com>
2017-05-21 15:22:43 -0400

    deletion of unnecessary content and a new design

---
+++ assets/script/nav.js
@@ -0,0 +1,17 @@
+var page = new Array("home", "works", "comm", "credit");
+var curr = 0;
+
+function goto(n) {
+  $("article#" + page[curr]).css('animation', 'goout 1s').css('opacity', 0).css('pointer-events', 'none');
+  setTimeout(function() {
+    $("article#" + page[n]).css('animation', 'goin 1s').css('opacity', 1).css('pointer-events', 'all');;
+  }, 750);
+  curr = n;
+}
+
+$(document).ready(function() {
+  $('article#home').load('content/about.html');
+  $('article#works').load('content/gallery.html');
+  $('article#comm').load('content/commission.html');
+  $('article#credit').load('content/credit.html');
+});
